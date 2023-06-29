35969d1febd04a42832db4b33905a7f97a17cfd4
Rajani VC <112753940+rajaniamin@users.noreply.github.com>
2023-06-29 17:01:48 +0530

    Joke generator

---
+++ day-1/task2chatgpt-1/src/main/java/com/masai/Task2chatgpt1Application.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task2chatgpt1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task2chatgpt1Application.class, args);
+	}
+
+}
---
+++ day-1/task2chatgpt-1/src/main/java/com/masai/controller/BotController.java
@@ -0,0 +1,43 @@
+package com.masai.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.masai.dto.ChatGptRequest;
+import com.masai.dto.ChatGptResponse;
+
+@RestController
+@RequestMapping("/bot")
+public class BotController implements WebMvcConfigurer {
+
+    @Value("${openai.model}")
+    private String model;
+
+    @Value(("${openai.api.url}"))
+    private String apiURL;
+
+    @Autowired
+    private RestTemplate template;
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam("prompt") String prompt) {
+        String str = "Tell me the joke on " + prompt;
+        ChatGptRequest request = new ChatGptRequest(model, str);
+        ChatGptResponse chatGptResponse = template.postForObject(apiURL, request, ChatGptResponse.class);
+        return chatGptResponse.getChoices().get(0).getMessage().getContent();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST")
+                .allowedHeaders("*");
+    }
+}
+
